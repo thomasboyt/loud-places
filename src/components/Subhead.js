a259d4474a447aeed45107dbb87aa4cb08b64931
Thomas Boyt <me@thomasboyt.com>
2018-05-23 20:03:33 -0400

    Support date ranges for events

--- src/components/Subhead.js
+++ src/components/Subhead.js
@@ -4,7 +4,13 @@ export default class Subhead extends React.Component {
   render() {
     const { frontmatter } = this.props
 
-    const items = [frontmatter.date]
+    const items = [];
+
+    if (frontmatter.end_date) {
+      items.push(`${frontmatter.date} - ${frontmatter.end_date}`)
+    } else {
+      items.push(frontmatter.date)
+    }
 
     if (frontmatter.location) {
       items.push(frontmatter.location)
@@ -37,6 +43,7 @@ export const markdownFrontmatterFragment = graphql`
   fragment SubheadFrontmatter on MarkdownRemark {
     frontmatter {
       date(formatString: "MMMM DD, YYYY")
+      end_date(formatString: "MMMM DD, YYYY")
       location
       cagematch
     }
